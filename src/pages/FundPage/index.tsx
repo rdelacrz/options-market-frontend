898c15ad8c883a6c07c00cc1ccd33f2536f93d08
Roger Delacruz <roger@inovas.net>
2021-05-06 17:41:23 -0400

    Test entries table to details page as well

--- src/pages/FundPage/index.tsx
+++ src/pages/FundPage/index.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { useParams } from 'react-router';
 import { FundBreadcrumbs, OptionsTableView } from '@components';
 import { Page } from '@layouts';
+import { OptionsEntry } from '@models';
 
 import './styles.scss';
 
@@ -12,10 +13,24 @@ interface PageProps {
 export const FundPage: FunctionComponent<PageProps> = (props) => {
     const params = useParams();
     const fundId = Number(params['id']);
+
+    // TO IMPLEMENT: REPLACE WITH ACTUAL DATA ONCE LINK TO ETH NETWORK IS COMPLETE
+    const entries: OptionsEntry[] = [
+        {
+            type: 'call', pair: 'WBTC-USDC', price: 60000, strike: 80000, expiration: new Date(), premium: 0.0592,
+            lp: null, share: 0, bop: null, wop: null, status: 'open', feature: null
+        },
+        {
+            type: 'put', pair: 'WBTC-ETH', price: 60000, strike: 80000, expiration: new Date(), premium: 0.0592,
+            lp: null, share: 0, bop: null, wop: null, status: 'open', feature: null
+        }
+    ]
+    const entry = entries[fundId];
+
     return (
         <Page className='fund-page-wrapper'>
             <FundBreadcrumbs className='fund-page-breadcrumbs' fundId={fundId} />
-            Fund Page
+            <OptionsTableView entries={[entry]} />
         </Page>
     );
 }
