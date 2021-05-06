44f7d60853f8c380fa57ffc1be01f601fdccd439
Roger Delacruz <roger@inovas.net>
2021-05-06 13:32:19 -0400

    Added minor logic for expiration date alignment

--- src/components/OptionsTableView/index.tsx
+++ src/components/OptionsTableView/index.tsx
@@ -3,6 +3,7 @@ import { ContextMenu, ContextMenuItem, DataView, IdentityBadge } from '@aragon/u
 import classnames from 'classnames';
 import { OptionsEntry } from '@models';
 import { useWindowSize } from '@utilities';
+import { format, utcToZonedTime } from 'date-fns-tz';
 
 import './styles.scss';
 
@@ -24,18 +25,22 @@ export const OptionsTableView: FunctionComponent<OptionsTableViewProps> = (props
     const { width } = useWindowSize();
     const mode = width > 900 ? 'table' : 'list';
     return (
-        <div>
+        <div className={classnames('options-table-view-wrapper', mode)}>
             <DataView
                 mode={mode}
                 fields={OPTIONS_FIELDS}
                 entries={props.entries}
                 renderEntry={({ type, pair, price, strike, expiration, premium, lp, share, bop, wop, status, feature }: OptionsEntry) => {
+                    const utcDate = utcToZonedTime(expiration, 'UTC');
                     return [
                         <IdentityBadge entity={type.toUpperCase()} />,
                         pair,
                         price,
                         strike,
-                        expiration.toString(),
+                        <div className='expiration-container'>
+                            <div className='date-text'>{format(utcDate, 'dd/MM/yyyy', { timeZone: 'UTC' })}</div>
+                            <div className='time-zone'>{format(utcDate, 'hh:mm', { timeZone: 'UTC' })} UTC</div>
+                        </div>,
                         premium,
                         lp,
                         share,
