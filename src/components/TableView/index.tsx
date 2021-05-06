fbb699629f5b353a88bb250006df86d748a9378a
Roger Delacruz <roger@inovas.net>
2021-05-06 11:24:55 -0400

    Added folder for TableView and services

---
+++ src/components/TableView/index.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent, useRef, useState } from 'react';
+import { Bar, Button, IconHome, IconConnect, IconSettings } from '@aragon/ui'
+import classnames from 'classnames';
+
+import './styles.scss';
+
+interface TableViewProps {
+    className?: string;
+}
+
+export const TableView: FunctionComponent<TableViewProps> = () => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
