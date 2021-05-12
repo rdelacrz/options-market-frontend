c8c834a2426d3740346d515362321b2a1dd1c2a3
Roger Delacruz <roger@inovas.net>
2021-05-12 11:27:18 -0400

    Fixed issues with NA

--- src/environment.ts
+++ src/environment.ts
@@ -4,7 +4,7 @@
 
 const environment = {
   useMockData: process.env.MOCK === 'true',
-  providerApiKey: 'RZYFMBGY7U1C1Q296XJ3EKGS7GMQZ556V5',
+  providerApiKey: 'RZYFMBGY7U1C1Q296XJ3EKGS7GMQZ556V5', // Replace this later
 };
 
 export default environment;
